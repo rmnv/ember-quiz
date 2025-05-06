f148d27a7e59415b7180e135208a5aa09e5738f0
Anton Romanov <anton@rmnv.ru>
2025-05-06 20:31:48 +0500

    feat: add navigation

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,8 +1,15 @@
 import Controller from '@ember/controller';
+import { service } from '@ember/service';
 
 export default class IndexController extends Controller {
+  @service router;
+
   get hasResults() {
     const results = JSON.parse(localStorage.getItem('quizResults') || '{}');
     return !!results.score;
+  }
+
+  get isHomePage() {
+    return this.router.currentRouteName === 'index';
   }
 } 
