fb80eaea5c465d2b66faf0b955ea773726bcfa0d
Anton Romanov <anton@rmnv.ru>
2025-05-06 17:05:05 +0500

    chore: tune eq helper, remove stylelinter

--- app/helpers/eq.js
+++ app/helpers/eq.js
@@ -1,7 +1,8 @@
 import { helper } from '@ember/component/helper';
 
-export function eq(params) {
-  return params[0] === params[1];
+export function eq([a, b]) {
+  return a === b;
 }
 
 export default helper(eq);
+
